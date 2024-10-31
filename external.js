76d431f3c3ba7808244794c22b6a4111bc101499
iiRooko <moskall_cody@hotmail.com>
2024-10-31 13:12:09 -0400

    Add: Logic for rock,paper,scissors for computer

--- external.js
+++ external.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    const num = Math.floor(Math.random() * 3);
+
+    switch (num) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        default:
+            return 'scissors';
+    }
+}
+
+console.log(getComputerChoice())
